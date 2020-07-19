c6d88ca20f873d787746389e031043d641501166
hitesh25kumar <hitesh25kumar.com>
2020-07-19 22:20:29 +0530

    Updates

---
+++ precache-manifest.0bf9110261831eee693994ae96e6bda3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb52c4a003b42e27ebb3fcd221215a4",
+    "url": "/hitesh-niyo-assignment/index.html"
+  },
+  {
+    "revision": "7291e8a76fcddb61d761",
+    "url": "/hitesh-niyo-assignment/static/css/main.ea7b077a.chunk.css"
+  },
+  {
+    "revision": "fa6ffb9fc458c0e43005",
+    "url": "/hitesh-niyo-assignment/static/js/2.ebe23fea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hitesh-niyo-assignment/static/js/2.ebe23fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7291e8a76fcddb61d761",
+    "url": "/hitesh-niyo-assignment/static/js/main.ee13326f.chunk.js"
+  },
+  {
+    "revision": "6bb94a0da7cc6accc526",
+    "url": "/hitesh-niyo-assignment/static/js/runtime-main.af9a1c12.js"
+  },
+  {
+    "revision": "4ae846194a4475e5a6d8b9a737aeeca8",
+    "url": "/hitesh-niyo-assignment/static/media/no-results.4ae84619.png"
+  },
+  {
+    "revision": "f45d0b0fcca3b0dbad130f185b383907",
+    "url": "/hitesh-niyo-assignment/static/media/no-results.f45d0b0f.gif"
+  },
+  {
+    "revision": "bb182cd14c0986dac7ca70b19f44951f",
+    "url": "/hitesh-niyo-assignment/static/media/placeholer.bb182cd1.jpg"
+  },
+  {
+    "revision": "154d2a4e8a3c3ecbbcd14a69e67de2c4",
+    "url": "/hitesh-niyo-assignment/static/media/search.154d2a4e.svg"
+  }
+]);
